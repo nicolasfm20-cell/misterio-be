9634612982744a6ad1f4fa09172503c5a6d87405
nicolasfm20-cell <nicolasfm20@gmail.com>
2026-06-24 12:57:06 -0300

    Atualizar o vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Mantém o nome original e a extensão do arquivo na pasta de assets
+        assetFileNames: 'assets/[name][extname]'
+      }
+    }
+  }
 })
