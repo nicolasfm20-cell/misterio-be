aa362d717131845498dca131486e35a827ce683e
nicolasfm20-cell <nicolasfm20@gmail.com>
2026-06-24 17:15:00 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        // Mantém o nome original e a extensão do arquivo na pasta de assets
-        assetFileNames: 'assets/[name][extname]'
-      }
-    }
-  }
+  base: '/', // Garante que a raiz do domínio seja respeitada na Vercel
 })
