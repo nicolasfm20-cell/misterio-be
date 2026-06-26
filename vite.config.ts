aba8d872f591a42f906e636b9a092ddb28ba2c9f
nicolasfm20-cell <nicolasfm20@gmail.com>
2026-06-25 22:40:20 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Garante que a raiz do domínio seja respeitada na Vercel
 })
